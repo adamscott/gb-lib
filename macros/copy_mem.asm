2bea0354d42ce3650efe5b3e62bc71c691a35c45
Adam Scott <ascott.ca@gmail.com>
2017-01-29 20:01:29 -0500

    Add blocks to copy_mem

--- macros/copy_mem.asm
+++ macros/copy_mem.asm
@@ -2,15 +2,35 @@
 ; hl = \1 = source address
 ; de = \2 = destination address
 ; bc = \3 = byte count
-copy_mem:   MACRO
-            push hl
-            push de
-            push bc
-            ld hl, \1       ; \1 = source address
-            ld de, \2       ; \2 = destination address
-            ld bc, \3       ; \3 = byte count
-            call memcpy
-            pop bc
-            pop de
-            pop hl
-            ENDM
+copy_mem:           MACRO
+                    copy_mem_logic \1, \2, \3, "both"
+                    ENDM
+
+copy_mem_begin:     MACRO
+                    copy_mem_logic \1, \2, \3, "begin"
+                    ENDM
+
+copy_mem_end:       MACRO
+                    copy_mem_logic \1, \2, \3, "end"
+                    ENDM
+
+copy_mem_middle:    MACRO
+                    copy_mem_logic \1, \2, \3, "middle"
+                    ENDM
+
+copy_mem_logic:     MACRO
+                    IF (STRIN(\4, "begin") || STRIN(\4, "both"))
+                        push af
+                        push bc
+                        push hl
+                    ENDC
+                    ld hl, \1       ; \1 = source address
+                    ld de, \2       ; \2 = destination address
+                    ld bc, \3       ; \3 = byte count
+                    call memcpy
+                    IF (STRIN(\4, "end") || STRIN(\4, "both"))
+                        pop hl
+                        pop bc
+                        pop af
+                    ENDC
+                    ENDM
