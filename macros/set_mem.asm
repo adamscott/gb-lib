e2a6a7c83d85b58fbc83ee4b306f1d389a2b2cc0
Adam Scott <ascott.ca@gmail.com>
2017-01-29 19:54:30 -0500

    Add block options to set_mem.

--- macros/set_mem.asm
+++ macros/set_mem.asm
@@ -2,15 +2,38 @@
 ; a  = \1 = byte value
 ; hl = \2 = destination address
 ; bc = \3 = byte count
-set_mem:    MACRO
-            push af
-            push hl
-            push bc
-            ld a, \1        ; \1 = byte value
-            ld hl, \2       ; \2 = destination address
-            ld bc, \3       ; \3 = byte count
-            call memset
-            pop bc
-            pop hl
-            pop af
-            ENDM
+set_mem:        MACRO
+                set_mem_logic \1, \2, \3, "both"
+                ENDM
+
+set_mem_begin:  MACRO
+                set_mem_logic \1, \2, \3, "begin"
+                ENDM
+
+set_mem_end:    MACRO
+                set_mem_logic \1, \2, \3, "end"
+                ENDM
+
+set_mem_middle: MACRO
+                set_mem_logic \1, \2, \3, "middle"
+                ENDM
+
+; block = \4 = block type ("begin", "end", "both", "middle")
+set_mem_logic:  MACRO
+                PRINTT "\4\n"
+                IF (STRIN(\4, "begin") || STRIN(\4, "both"))
+                    push af
+                    push hl
+                    push bc
+                ENDC
+                ld a, \1        ; \1 = byte value
+                ld hl, \2       ; \2 = destination address
+                ld bc, \3       ; \3 = byte count
+                call memset
+                IF (STRIN(\4, "end") || STRIN(\4, "both"))
+                    PRINTT "end?\n"
+                    pop bc
+                    pop hl
+                    pop af
+                ENDC
+                ENDM
